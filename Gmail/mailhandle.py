ea46fa382b8507c276494bb595a1d54fa8d9c98e
Anjan Nair <22571613+anjannair@users.noreply.github.com>
2020-09-09 16:07:31 +0530

    Updated to check login

--- Gmail/mailhandle.py
+++ Gmail/mailhandle.py
@@ -1,11 +1,16 @@
 #https://developers.google.com/gmail/api/v1/reference/ get the credentials.json file from here
-import ezgmail, os
+import ezgmail, os, time
 #os.chdir(r'credentials.json')      #use these first after downloading the credentials.json file 
-#ezgmail.init()                     #after that comment it 
-file1 = open('mail_to_send.txt', 'r')
-Lines = file1.readlines()
-for line in Lines:
-    ezgmail.send(line.strip(), 'Waddup', 'Hello')   #title,body
+#ezgmail.init()                     #after that comment it
+if ezgmail.LOGGED_IN == True:
+    print("Logged in using "+ ezgmail.EMAIL_ADDRESS)
+    time.sleep(3)
+    file1 = open('mail_to_send.txt', 'r')
+    Lines = file1.readlines()
+    for line in Lines:
+        ezgmail.send(line.strip(), 'Waddup', 'Hello')   #title,body
 #ezgmail.send('recipient@example.com', 'Subject line', 'Body of the email',         #These are for cc and bccing
 #cc='friend@example.com', bcc='otherfriend@example.com,someoneelse@example.com')
-print("DONE!")
+    print("DONE!")
+else:
+    print("Logging in failed. Check if correct credentials.json file is loaded and check if token.json is generated after line 2 and 3")
